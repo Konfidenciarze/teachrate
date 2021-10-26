b7e86c1df7baa609667763469aea5c68f34abdf6
quqacz <67021265+quqacz@users.noreply.github.com>
2021-10-26 09:35:40 +0200

    db connection

--- app.ts
+++ app.ts
@@ -1,9 +1,28 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
+
 import express from "express";
 import { Auth } from './routes/auth';
 import { User } from './routes/user';
 
+const mongoose = require('mongoose');
+
 const app = express();
 const port = 3000;
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/teachrater';
+
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, 'connection error:'));
+db.once("open", ()=>{
+    console.log("Database connected");
+})
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
