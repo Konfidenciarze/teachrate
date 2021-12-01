5f510e9c0efa97bd27b38c2fef41956a4bb5ba0d
quqacz <67021265+quqacz@users.noreply.github.com>
2021-12-01 08:13:20 +0100

    .

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const UserSchema = new Schema({
-    username: { type: String, reqired: true, unique: true},
+    username: { type: String, reqired: true},
     password: {type: String},
     prime: {type: String, default: ''},
     secondary: {type: String, default: ''}
