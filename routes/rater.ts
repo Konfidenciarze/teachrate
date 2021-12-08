a3df39b41ff95ffc734c0b2e03c7642c836fbb0e
quqacz <67021265+quqacz@users.noreply.github.com>
2021-12-08 10:24:09 +0100

    .

--- routes/rater.ts
+++ routes/rater.ts
@@ -1,6 +1,7 @@
 import express, {Request, Response}  from "express";
 const Rate = express.Router();
 const Teacher = require('../models/teacher')
+const Rating = require('../models/rating')
 
 Rate.get('/', (req: Request, res: Response)=>{
     res.render('browser', {teachers: []});
@@ -14,6 +15,16 @@ Rate.post('/browse', async (req: Request, res: Response)=>{
     const {name} = req.body;
     const teachers = await Teacher.find({data: {"$regex": name, '$options' : 'i'}})
     res.render('browser', {teachers});
+})
+
+Rate.post('/:teacherId/by/:userId', async(req: Request, res: Response)=>{
+    try{
+        const { material, punctual, passing, comment } = req.body
+        const teacher = await Teacher.findOne({_id: req.params.teacherId})
+        res.end()
+    }catch(e){
+        console.log(e)
+    }
 })
 
 Rate.get('/profile/:id', async(req: Request, res: Response)=>{
